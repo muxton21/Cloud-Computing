f650fd13d947ff44850c4fc7f4ca4ef2f253e0dc
Matthew Gabriel <muxton212121@gmail.com>
2018-11-19 16:38:34 +0000

    change

--- WordCount.java
+++ WordCount.java
@@ -21,9 +21,8 @@ public class WordCount {
 
     public void map(Object key, Text value, Context context
                     ) throws IOException, InterruptedException {
-      StringTokenizer itr = new StringTokenizer(value.toString());
+      StringTokenizer itr = new StringTokenizer(value.toString().replaceAll("[^a-z]+", ""));
       while (itr.hasMoreTokens()) {
-            word.toLowerCase().replaceAll("[^a-z]+", "");
             word.set(itr.nextToken());
             context.write(word, one);
       }
