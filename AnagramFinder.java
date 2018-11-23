3c005faac0c0386b344c741d371e2690eda1abbb
Matthew Gabriel <muxton212121@gmail.com>
2018-11-23 12:16:32 +0000

    changes

--- AnagramFinder.java
+++ AnagramFinder.java
@@ -77,11 +77,11 @@ public class AnagramFinder {
             //puts the entire text of the book into a string
             String line = value.toString();
             //split the string into an array of all words in the book
-            String[] splitString = line.split("[^-a-zA-Z']");
+            String[] splitString = line.split("(?!--)[^-a-zA-Z']");
 
             //remove any punctuation
             for(int i=0;i<splitString.length;i++){
-                splitString[i] = splitString[i].toLowerCase().replaceAll("[^a-z ]", "");
+                splitString[i] = splitString[i].toLowerCase().trim().replaceAll("[^-a-zA-ZAÁ-ÖØ-öø-ʸ'`\\w\\s]", "");;
             }
 
             //set each word to the key value pair in the mapper
